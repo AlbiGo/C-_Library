ed3db6c80d0f9d2ac9db61262f23ebff3ed0548e
Albi Goxhaj <albi.goxhaj@pwc.com>
2025-02-26 11:07:20 +0100

    Test commit

--- AdvancedFeatures/Linq/LinqExt.cs
+++ AdvancedFeatures/Linq/LinqExt.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdvancedFeatures.Linq
+﻿namespace AdvancedFeatures.Linq
 {
+    /// <summary>
+    /// Linq extension
+    /// </summary>
     public static class LinqExt
     {
-        public static IEnumerable<int> WherePositive(this IEnumerable<int> myLst, Func<int,bool> func)
+        public static IEnumerable<int> WherePositive(this IEnumerable<int> myLst, Func<int, bool> func)
         {
             foreach (int element in myLst)
                 if (func(element))
@@ -16,7 +13,6 @@ namespace AdvancedFeatures.Linq
                     yield return element;
                     myLst.GetEnumerator().MoveNext();
                 }
-                    
         }
     }
 }
